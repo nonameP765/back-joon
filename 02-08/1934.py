9d297fb4b4c48cc2196fba7f0b1ad90e84287d64
noname_Producer <nonamep765@gmail.com>
2019-02-08 23:38:27 +0900

    change...

--- 02-08/1934.py
+++ 02-08/1934.py
@@ -1,40 +1,17 @@
-d, c, r = map(int, input().split(' '))
+for i in range(int(input())):
+    _a, _b = map(int, input().split(' '))
+
+    a, b = (_a, _b) if _a > _b else (_b, _a)
+
+    mi = b
+
+    while a % b != 0:
+        tmp = b
+        b = a % b
+        a = tmp
+    else:
+        mi = b
+
+    print((_a // mi) * _b)
 
 
-graph = dict()
-for i in range(1, d+1):
-    graph[i] = list()
-
-for i in range(c):
-    a, b = map(int, input().split(' '))
-    graph[a].append(b)
-    graph[b].append(a)
-
-for i in range(d):
-    graph[i+1].sort()
-
-# dfs
-visited = [r]
-to_visit = [r]
-
-while to_visit:
-    now = to_visit.pop()
-    for i in (set(graph[now])-set(visited)):
-        visited.append(i)
-        to_visit.append(i)
-        break
-for i in visited:
-    print(i, end=' ')
-print()
-# bfs
-visited = [False for i in range(d)]
-visited[r-1] = True
-to_visit = [r]
-
-while to_visit:
-    now = to_visit.pop(0)
-    print(now, end=' ')
-    for i in graph[now]:
-        if not visited[i - 1]:
-            visited[i - 1] = True
-            to_visit.append(i)
