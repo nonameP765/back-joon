2b123172edbd3a48d17db7f6c349c2b899692942
noname_Producer <nonamep765@gmail.com>
2019-02-13 23:17:50 +0900

    02-13

---
+++ 02-13/13241.py
@@ -0,0 +1,16 @@
+_a, _b = map(int, input().split(' '))
+
+a, b = (_a, _b) if _a > _b else (_b, _a)
+
+mi = b
+
+while a % b != 0:
+    tmp = b
+    b = a % b
+    a = tmp
+else:
+    mi = b
+
+print((_a // mi) * _b)
+
+
