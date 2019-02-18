4ecc002b47c9fa32b3fd65d6ed5f1fdf4b1ed567
noname_Producer <nonamep765@gmail.com>
2019-02-18 23:24:10 +0900

    02-18 info

--- 02-18/1912_2.py
+++ 02-18/1912_2.py
@@ -1,14 +1,18 @@
 c = input()
 a = list(map(int, input().split(' ')))
 
+# 이거때문에 런타임에러로 고생
 if c == '1':
     print(a[0])
 else:
+    # 최고 수
     mx = a[0] + a[1]
+    # 현재 합
     n = 0
     for i in a:
         n += i
         mx = mx if mx > n else n
+        # 만약 합이 0아래면 다음부터 다시 합함
         if n < 0:
             n = 0
 
