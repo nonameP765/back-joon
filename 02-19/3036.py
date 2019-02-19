7a27de30823dc24c4f4d45ea1c8a00fa31b502f6
noname_Producer <nonamep765@gmail.com>
2019-02-19 23:35:39 +0900

    02-19 info

--- 02-19/3036.py
+++ 02-19/3036.py
@@ -8,7 +8,7 @@ for i in range(1, c):
     a = m
     b = inp[i]
     mi = b
-
+    # 최대공약수 구하기
     while a % b != 0:
         tmp = b
         b = a % b
@@ -16,6 +16,7 @@ for i in range(1, c):
     else:
         mi = b
 
+    # 첫번째 톱니//최대공약수, 구할 톱니//최대공약수
     print(str(m // mi)+'/'+str(inp[i]//mi))
 
 
