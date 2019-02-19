2a5b17506a752f7f57befaa5ee48a19588d73ac8
noname_Producer <nonamep765@gmail.com>
2019-02-20 04:17:26 +0900

    02-20 info

--- 02-20/1520.py
+++ 02-20/1520.py
@@ -3,26 +3,28 @@ l = list()
 for i in range(a):
     l.append(list(map(int, input().split(' '))))
 
-success = [[0 for i in range(b)] for j in range(a)]
+success = [[-1 for i in range(b)] for j in range(a)]
+success[-1][-1] = 1
 
 
 def s(i, j):
     if i == a - 1 and j == b - 1:
         return
-    if success[i][j] == 0:
+    if success[i][j] == -1:
         if j < b - 1 and l[i][j] > l[i][j + 1]:
             s(i, j + 1)
-            success[i][j] += success[i][j + 1]
+            success[i][j] += success[i][j + 1] + 1
         if i < a - 1 and l[i][j] > l[i + 1][j]:
             s(i + 1, j)
-            success[i][j] += success[i + 1][j]
+            success[i][j] += success[i + 1][j] + 1
         if i > 0 and l[i][j] > l[i - 1][j]:
             s(i - 1, j)
-            success[i][j] += success[i - 1][j]
+            success[i][j] += success[i - 1][j] + 1
         if j > 0 and l[i][j] > l[i][j - 1]:
             s(i, j - 1)
-            success[i][j] += success[i][j - 1]
+            success[i][j] += success[i][j - 1] + 1
 
 
 s(0, 0)
+print(success)
 print(success[0][0])
--- 02-20/1520_remake.py
+++ 02-20/1520_remake.py
@@ -3,20 +3,22 @@ l = list()
 for i in range(a):
     l.append(list(map(int, input().split(' '))))
 
-success = [[-1 for i in range(b)] for j in range(a)]
+# dp할 배열 생성
+success = [[None for i in range(b)] for j in range(a)]
 
 
 def s(i, j):
-    # 이부분도 중요함 여길 안막으면 종점에 가서도 역으로 재탐색함
+    # 이부분도 중요함 여길 안막으면 종점에 가서도 역으로 재탐색함(이거도 애매했음)
     if i == a - 1 and j == b - 1:
         return 1
-
-    if success[i][j] >= 0:
+    # 이미 탐색 됬다면(None 이 아니니까) 그냥 리턴
+    if success[i][j] is not None:
         return success[i][j]
-    if success[i][j] == -1:
-        success[i][j] = 0
 
-    # 상하좌우 전부로 움직일 수 있음(이거때문에 자꾸 틀림)
+    # 탐색 시작시 초기화
+    success[i][j] = 0
+
+    # 상하좌우 전부로 움직일 수 있음(이거때문에 계속 틀림)
     if j < b - 1 and l[i][j] > l[i][j + 1]:
         success[i][j] += s(i, j + 1)
     if j > 0 and l[i][j] > l[i][j - 1]:
@@ -30,4 +32,3 @@ def s(i, j):
 
 
 print(s(0, 0))
-
