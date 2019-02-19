099eb8fe6460ae2977808f08eade0b7807ec9b06
noname_Producer <nonamep765@gmail.com>
2019-02-20 02:35:44 +0900

    02-20 why why why?

--- 02-20/1520.py
+++ 02-20/1520.py
@@ -3,19 +3,25 @@ l = list()
 for i in range(a):
     l.append(list(map(int, input().split(' '))))
 
+success = [[0 for i in range(b)] for j in range(a)]
+success[-1][-1] = 1
+
 
 def s(i, j):
-    if i == a-1 and j == b-1:
-        return 1
-    re = 0
-    if i < a - 1 and l[i][j] > l[i + 1][j]:
-        re += s(i + 1, j)
-    if j > 0 and l[i][j] > l[i][j - 1]:
-        re += s(i, j - 1)
-    if j < b - 1 and l[i][j] > l[i][j + 1]:
-        re += s(i, j + 1)
+    if success[i][j] == 0:
+        if j < b - 1 and l[i][j] > l[i][j + 1]:
+            s(i, j + 1)
+            if success[i][j + 1] > 0:
+                success[i][j] += success[i][j + 1]
+        if i < a - 1 and l[i][j] > l[i + 1][j]:
+            s(i + 1, j)
+            if success[i + 1][j] > 0:
+                success[i][j] += success[i + 1][j]
+        if j > 0 and l[i][j] > l[i][j - 1]:
+            s(i, j - 1)
+            if success[i][j - 1] > 0:
+                success[i][j] += success[i][j - 1]
 
-    return re
 
-
-print(s(0, 0))
+s(0, 0)
+print(success[0][0])
