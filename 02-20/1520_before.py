a2aa6bf56fd4c7d6fee0d715c5b0606e8d6a1fb6
noname_Producer <nonamep765@gmail.com>
2019-02-20 04:24:46 +0900

    02-20 before info

--- 02-20/1520_before.py
+++ 02-20/1520_before.py
@@ -3,17 +3,22 @@ l = list()
 for i in range(a):
     l.append(list(map(int, input().split(' '))))
 
+# dp할 배열 만들기
 success = [[None for i in range(b)] for j in range(a)]
+# success 내 값을 사용해서 종점을 미리 표시
 success[-1][-1] = 1
 
 
 def s(i, j):
-    if i == a - 1 and j == b - 1:
-        return
-    if success[i][j] is None:
+    # 첫번쨰 탐색이고, 종점이 아닐때
+    if success[i][j] is None and not (i == a - 1 and j == b - 1):
         success[i][j] = 0
+
+        # 상하좌우 탐색
         if j < b - 1 and l[i][j] > l[i][j + 1]:
+            # 탐색
             s(i, j + 1)
+            # 한 뒤에 저장된 결과값을 지금에 더함
             success[i][j] += success[i][j + 1]
         if i < a - 1 and l[i][j] > l[i + 1][j]:
             s(i + 1, j)
