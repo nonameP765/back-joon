234ae42cc4d64bf3dda647e94dc8da3bd14bf984
noname_Producer <nonamep765@gmail.com>
2019-02-23 20:04:55 +0900

    11052 info

--- 2019-02/02-23/11052.py
+++ 2019-02/02-23/11052.py
@@ -1,9 +1,13 @@
 a = int(input())
 l = list(map(int, input().split(' ')))\
 
+# dp용 배열
 dp = [0, l[0]]
 
 for i in range(2, a + 1):
+    # 이전까지의 계산결과를 dp에 저장하고
+    # l[i] 와
+    # l[1] + l[i - 1], l[2] + [i - 2].... 을 비교해서 가장 큰걸 dp[i]로 넣는다.
     dp.append(max([dp[z+1] + dp[i - z - 1] for z in range(i//2)] + [l[i - 1]]))
 
 print(dp[-1])
