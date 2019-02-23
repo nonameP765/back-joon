9b240d38418c78f23e0078e0846a70f6dcba472d
noname_Producer <nonamep765@gmail.com>
2019-02-23 20:04:19 +0900

    11052 optimization

--- 2019-02/02-23/11052.py
+++ 2019-02/02-23/11052.py
@@ -4,6 +4,6 @@ l = list(map(int, input().split(' ')))\
 dp = [0, l[0]]
 
 for i in range(2, a + 1):
-    dp.append(max([dp[z] + dp[i - z] for z in range(1, i)] + [l[i - 1]]))
+    dp.append(max([dp[z+1] + dp[i - z - 1] for z in range(i//2)] + [l[i - 1]]))
 
 print(dp[-1])
