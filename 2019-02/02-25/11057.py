66bb7a2baa9ca5a07c8f5d71e7b371650337a8f8
noname_Producer <nonamep765@gmail.com>
2019-02-25 18:48:02 +0900

    11057 info

--- 2019-02/02-25/11057.py
+++ 2019-02/02-25/11057.py
@@ -1,8 +1,10 @@
 a = int(input())
 
+# dp용 배열
 dp = [None, [1 for i in range(10)]]
 
 for i in range(2, a + 1):
+    # 높은 index의 값을 다 더해서 저장
     dp.append([sum(dp[i - 1][z:]) for z in range(10)])
 
 print(sum(dp[a]) % 10007)
