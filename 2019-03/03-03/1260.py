272e31a3c99a7ec5f3b5bde5de7f166b24e609a0
noname_Producer <nonamep765@gmail.com>
2019-03-03 16:27:16 +0900

    1260 success

--- 2019-03/03-03/1260.py
+++ 2019-03/03-03/1260.py
@@ -14,27 +14,32 @@ for i in range(d):
     graph[i+1].sort()
 
 # dfs
-visited = [r]
-to_visit = [r]
+stack = [r]
+visited = [False for i in range(d + 1)]
 
-while to_visit:
-    now = to_visit.pop()
-    for i in (set(graph[now])-set(visited)):
-        visited.append(i)
-        to_visit.append(i)
-        break
-for i in visited:
-    print(i, end=' ')
+while stack:
+    now = stack.pop()
+    if not visited[now]:
+        visited[now] = True
+        print(now, end=' ')
+    tmp = graph[now][:]
+    tmp.sort(reverse=True)
+    for i in tmp:
+        if not visited[i]:
+            stack.append(i)
+
 print()
 # bfs
-visited = [False for i in range(d)]
-visited[r-1] = True
-to_visit = [r]
+q = [r]
+visited = [False for i in range(d + 1)]
 
-while to_visit:
-    now = to_visit.pop(0)
-    print(now, end=' ')
-    for i in graph[now]:
-        if not visited[i - 1]:
-            visited[i - 1] = True
-            to_visit.append(i)
+while q:
+    now = q.pop(0)
+    if not visited[now]:
+        visited[now] = True
+        print(now, end=' ')
+    tmp = graph[now][:]
+    tmp.sort()
+    for i in tmp:
+        if not visited[i]:
+            q.append(i)
