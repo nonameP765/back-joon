276ff76d984c4c98ef009da50a45559282454370
noname_Producer <nonamep765@gmail.com>
2019-03-06 11:19:39 +0900

    2667 info

--- 2019-03/03-06/2667 dfs.py
+++ 2019-03/03-06/2667 dfs.py
@@ -1,41 +1,38 @@
 c = int(input())
 l = [list(map(int, input())) for i in range(c)]
 
+# dfs용 방문확인배열
 visited = [[False for j in range(c)] for i in range(c)]
 
-
-count = 0
+# 결과값 저장용
 result = list()
 for i in range(c):
     for j in range(c):
+        # 2중 반복문으로 방문하지 않은 모든 점을 방문함
+        # 만약 방문하지 않고, 집이 있다면
         if not visited[j][i] and l[j][i] == 1:
+            # 현재 카운트
+            count = 0
+            # 스택으로 dfs탐색 시작
             stack = [(i, j)]
             while stack:
                 a, b = stack.pop()
+                # 방문하지 않았다면
                 if not visited[b][a]:
-                    if l[b][a] == 0:
-                        if count != 0:
-                            result.append(count)
-                            count = 0
-
-                    if l[b][a] == 1:
-                        count += 1
-                        if 0 < a and l[b][a - 1] == 1:
-                            stack.append((a - 1, b))
-                        if 0 < b and l[b - 1][a] == 1:
-                            stack.append((a, b - 1))
-                        if a < c - 1 and l[b][a + 1] == 1:
-                            stack.append((a + 1, b))
-                        if b < c - 1 and l[b + 1][a] == 1:
-                            stack.append((a, b + 1))
+                    # 집 개수 1 증가
+                    count += 1
+                    # 위아래좌우 값이 1인경우 스택에 추가
+                    if 0 < a and l[b][a - 1] == 1:
+                        stack.append((a - 1, b))
+                    if 0 < b and l[b - 1][a] == 1:
+                        stack.append((a, b - 1))
+                    if a < c - 1 and l[b][a + 1] == 1:
+                        stack.append((a + 1, b))
+                    if b < c - 1 and l[b + 1][a] == 1:
+                        stack.append((a, b + 1))
                     visited[b][a] = True
-                if len(stack) == 0:
-                    if count != 0:
-                        result.append(count)
-                        count = 0
+            result.append(count)
             visited[j][i] = True
-
-
 
 result.sort()
 print(len(result))
