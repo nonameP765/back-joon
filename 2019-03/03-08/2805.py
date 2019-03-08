d845164503c4915604b772a7282d1bc770dd9927
noname_Producer <nonamep765@gmail.com>
2019-03-08 13:13:23 +0900

    2805 info

--- 2019-03/03-08/2805.py
+++ 2019-03/03-08/2805.py
@@ -2,12 +2,15 @@ c, r = map(int, input().split(' '))
 l = list(map(int, input().split(' ')))
 l.sort()
 
+# 나무의 길이는 0부터
 left = 0
+# 최고로 큰 나무까지 가능하다
 right = l[-1]
 re = 0
 while left <= right:
     mid = (left + right) // 2
     count = 0
+    # 탐색하는 중간값을 기준으로 나무를 자르고 저장
     for i in l:
         if i > mid:
             count += i - mid
