3bab4b780d1484e3cadf596cb19ea1ce21202470
noname_Producer <nonamep765@gmail.com>
2019-03-11 12:18:42 +0900

    11724 info

--- 2019-03/03-11/11724.py
+++ 2019-03/03-11/11724.py
@@ -1,4 +1,5 @@
 c, n = map(int, input().split(' '))
+# 그래프 제작
 graph = {i: list() for i in range(1, c + 1)}
 
 for i in range(n):
@@ -9,8 +10,9 @@ for i in range(n):
 visited = [False for i in range(c + 1)]
 count = 0
 
-
+# 모든 정점을 순환
 for j in range(1, c + 1):
+    # 물론 방문하지 않은 경우만
     if not visited[j]:
         stack = [j]
 
