68c1efba53c08266a7d53e0d5a8cf7f23615b934
noname_Producer <nonamep765@gmail.com>
2019-03-11 12:17:49 +0900

    2468 info

--- 2019-03/03-11/2468.py
+++ 2019-03/03-11/2468.py
@@ -2,23 +2,26 @@ c = int(input())
 
 l = [list(map(int, input().split(' '))) for i in range(c)]
 
+# 0부터 최대값까지 탐색해보기 위해 구함
 m = 0
 for i in range(c):
     for j in range(c):
         m = max(m, l[i][j])
 
+# 결과값
 mx = 0
-mx_num = 0
 
-
+# 0부터 최대값까지 탐색
 for k in range(m + 1):
     visited = [[False for j in range(c)] for i in range(c)]
     n = 0
     for i in range(c):
         for j in range(c):
             if not visited[i][j]:
+                # 만약 탐색중인 수보다 크면 DFS
                 if l[i][j] > k:
                     stack = [(i, j)]
+                    # 대피소? 영역 1씩 추가
                     n += 1
                     while stack:
                         a, b = stack.pop()
