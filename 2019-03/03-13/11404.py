fe68fa2bb1ba5c412fd584eaeb364e86dafe4357
noname_Producer <nonamep765@gmail.com>
2019-03-13 14:51:39 +0900

    11404 info

--- 2019-03/03-13/11404.py
+++ 2019-03/03-13/11404.py
@@ -3,17 +3,20 @@ m = int(input())
 
 INF = 1e9
 
+# 그래프
 graph = {i: {j: INF for j in range(1, n + 1)} for i in range(1, n + 1)}
 
 for _ in range(m):
     a, b, c = map(int, input().split(' '))
 
+    # 똑같은 간선인 길이가 더 길어버리기~
     if graph[a].get(b, False):
         graph[a][b] = min(graph[a][b], c)
     else:
         graph[a][b] = c
 
 
+# 3중 반복문으로 간선 체크
 for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
@@ -21,6 +24,7 @@ for k in range(1, n + 1):
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
+        # 만약 자신이면 0
         if i == j:
             print(0, end=' ')
         else:
