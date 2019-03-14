063c119af748f5f2bfa4b9224d867f747acd17a7
nonamep765 <nonamep765@gmail.com>
2019-03-14 13:29:43 +0900

    2606 info

--- 2019-03/03-14/2606.py
+++ 2019-03/03-14/2606.py
@@ -5,11 +5,14 @@ graph = {i: list() for i in range(1, N + 1)}
 
 for _ in range(M):
     a, b = map(int, input().split(' '))
+    # 양방향임 이거때문에 몇번 재채점함
     graph[a].append(b)
     graph[b].append(a)
 
 check = {i: False for i in range(1, N + 1)}
 
+# bfs 구현 이게 왜 최단거리?
+# 플로이드 와샬 쓰면 n^3인데..
 q = [1]
 count = 0
 
