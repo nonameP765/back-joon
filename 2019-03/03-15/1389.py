4dbe5c10b03e116d62e0c0bf4783d248ce451788
noname_Producer <nonamep765@gmail.com>
2019-03-15 10:21:24 +0900

    1389 success

---
+++ 2019-03/03-15/1389.py
@@ -0,0 +1,45 @@
+from heapq import heappush, heappop
+
+INF = 1e9
+
+N, M = map(int, input().split(' '))
+
+graph = {i: list() for i in range(1, N + 1)}
+
+for _ in range(M):
+    a, b = map(int, input().split(' '))
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+def search(start):
+    hq = list()
+    visited = {i: False for i in range(1, N + 1)}
+    check = {i: INF for i in range(1, N + 1)}
+    heappush(hq, (0, start))
+    while hq:
+        w, now = heappop(hq)
+        if check[now] < w:
+            continue
+        visited[now] = True
+        for nxt in graph[now]:
+            w2 = w + 1
+            if w2 < check[nxt]:
+                check[nxt] = w2
+                heappush(hq, (w2, nxt))
+    c = 0
+    for i in range(1, N + 1):
+        if i == start:
+            continue
+        c += check[i]
+    return c
+
+
+min_num = 1
+min = search(1)
+for i in range(2, N + 1):
+    if min > search(i):
+        min_num = i
+        min = search(i)
+
+print(min_num)
