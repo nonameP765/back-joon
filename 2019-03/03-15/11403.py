be62a2096b77dbfcb355233440e98bdc7c7146c9
noname_Producer <nonamep765@gmail.com>
2019-03-15 10:37:10 +0900

    11403 info

--- 2019-03/03-15/11403.py
+++ 2019-03/03-15/11403.py
@@ -2,6 +2,7 @@ c = int(input())
 
 graph = [list(map(int, input().split(' '))) for _ in range(c)]
 
+# 플로이드 워셜 알고리즘이라 쓰고 3중 반복문이라 읽는다
 for k in range(c):
     for i in range(c):
         for j in range(c):
