bda3ea26508938930ef806b41abdd137a0f49ceb
noname_Producer <nonamep765@gmail.com>
2019-03-17 23:10:18 +0900

    6118 info

--- 2019-03/03-17/6118.py
+++ 2019-03/03-17/6118.py
@@ -8,16 +8,22 @@ graph = {i: list() for i in range(1, N + 1)}
 
 for _ in range(M):
     a, b = map(int, input().split(' '))
+    # 왠지 순서대로 처리해야할거 같아서 힙 썼는데 필요없...
     heappush(graph[a], b)
     heappush(graph[b], a)
 
 check = {i: INF for i in range(1, N + 1)}
 
+# 우선순위 큐 구현
 hq = list()
 heappush(hq, (0, 1))
 
+# 높은 값
 mx = 0
+# 높은 위치
 mxn = 0
+# 높은 위치들
+# 중복되지 않게 하려고 집합으로 구현
 mxc = set()
 while hq:
     w, now = heappop(hq)
