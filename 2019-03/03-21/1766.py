d85c4048303f8262d3817c4ac1c837852f9a0ca9
noname_Producer <nonamep765@gmail.com>
2019-03-21 09:00:38 +0900

    1766 success

---
+++ 2019-03/03-21/1766.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+
+graph = {i: list() for i in range(1, N + 1)}
+count = {i: 0 for i in range(1, N + 1)}
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    count[b] += 1
+
+queue = list()
+for i in range(1, N + 1):
+    if count[i] == 0:
+        queue.append(i)
+
+result = ''
+for _ in range(N):
+    queue.sort()
+    now = queue.pop(0)
+    result += str(now) + ' '
+    for nxt in graph[now]:
+        count[nxt] -= 1
+        if count[nxt] == 0:
+            queue.append(nxt)
+
+print(result)
